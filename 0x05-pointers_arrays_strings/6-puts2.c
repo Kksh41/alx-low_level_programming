dca22b80f417578d0fd255b3f41d817505f8d185
Kksh41 <mn.live42@gmail.com>
2024-01-04 01:09:03 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+#include <stdio.h> // Include the necessary header file
 
 /**
  * puts2 - Prints every other character from the string, starting with the first.
@@ -26,8 +26,9 @@ void puts2(char *str)
     for (o = 0; o <= t; o++)
     {
         if (o % 2 == 0)
-            _putchar(str[o]);
+            putchar(str[o]); // Changed _putchar to putchar
     }
-    _putchar('\n');
+    putchar('\n'); // Changed _putchar to putchar
 }
+
 
