f3c67eebba549f6e0639716f0accace4abe04514
Kksh41 <mn.live42@gmail.com>
2024-01-03 23:02:19 -0800

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,18 @@
+#indef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+#include < unistd.h>
+
+int _putchar(char c);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *5);
+void _puts (char *str);
+void print_rev(char *5);
+void rev_string(char *5);
+void puts2(char*str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_stropy(char *dest, char *src);
+
+#endif
